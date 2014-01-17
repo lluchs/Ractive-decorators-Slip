7850c4e7ad61e3516414bab1dccecf9a1860eff6
Luchs <Luchs@euirc.eu>
2014-01-17 23:45:58 +0100

    Implement reordering

--- src/Ractive-decorators-Slip.js
+++ src/Ractive-decorators-Slip.js
@@ -36,27 +36,70 @@
 
 	// Common JS (i.e. browserify) environment
 	if ( typeof module !== 'undefined' && module.exports && typeof require === 'function' ) {
-		factory( require( 'Ractive' ) );
+		factory( require( 'Ractive' ), require( 'slip' ) );
 	}
 
 	// AMD?
 	else if ( typeof define === 'function' && define.amd ) {
-		define([ 'Ractive' ], factory );
+		define([ 'Ractive', 'slip' ], factory );
 	}
 
 	// browser global
-	else if ( global.Ractive ) {
-		factory( global.Ractive );
+	else if ( global.Ractive && global.Slip ) {
+		factory( global.Ractive, global.Slip );
 	}
 
 	else {
-		throw new Error( 'Could not find Ractive! It must be loaded before the Ractive-decorators-Slip plugin' );
+		throw new Error( 'Could not find Ractive or Slip! They must be loaded before the Ractive-decorators-Slip plugin' );
 	}
 
-}( typeof window !== 'undefined' ? window : this, function ( Ractive ) {
+}( typeof window !== 'undefined' ? window : this, function ( Ractive, Slip ) {
 
 	'use strict';
 
-	/* plugin code goes here */
+	var errorMessage;
+
+	errorMessage = 'The Slip decorator only works with elements that correspond to array members';
+
+	Ractive.decorators.Slip = function ( node ) {
+		var slip, ractive;
+
+		slip = new Slip( node );
+
+		node.addEventListener( 'slip:reorder', reorder, false );
+		
+		function reorder( e ) {
+			var storage = e.target._ractive, lastDotIndex, sourceKeypath, sourceArray, sourceIndex;
+			var array, source;
+
+			sourceKeypath = storage.keypath;
+
+			// this decorator only works with array members!
+			lastDotIndex = sourceKeypath.lastIndexOf( '.' );
+
+			if ( lastDotIndex === -1 ) {
+					throw new Error( errorMessage );
+			}
+
+			sourceArray = sourceKeypath.substr( 0, lastDotIndex );
+			sourceIndex = +( sourceKeypath.substring( lastDotIndex + 1 ) );
+
+			ractive = storage.root;
+
+			array = ractive.get( sourceArray );
+
+			// remove source from array
+			source = array.splice( sourceIndex, 1 )[0];
+			// add source back to array in new location
+			array.splice( e.detail.spliceIndex, 0, source );
+		}
+
+		return {
+			teardown: function () {
+				slip.detach();
+				node.removeEventListener( 'slip:reorder', reorder, false );
+			}
+		};
+	};
 
 }));
